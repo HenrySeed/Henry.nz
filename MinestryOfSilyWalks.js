c9cacc8079d917b622fa00e8e47b26b09dc30bc5
Henry Seed <henry@jaipuna.com>
2019-09-22 10:14:35 +1200

    new favicon, changes snake behaviour

--- MinestryOfSilyWalks.js
+++ MinestryOfSilyWalks.js
@@ -1,27 +1,57 @@
 let xPos;
 let yPos;
 
-let c = 0;
+let c;
 
 const movement = 1;
 const ellipseWidth = 90;
-let rainbowRaising = true;
+
+let canvasActive = true;
+
+let mousehasntMoved = true;
+let ogMousePos;
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("sketchHolder");
   loop();
 
-  colorMode(HSB);
+  colorMode(HSB, 255);
 
   noStroke();
   fill(66, 245, 215);
 
+  c = random(0, 255);
+
   xPos = width / 2;
   yPos = height / 2;
+
+  ogMousePos = [mouseX, mouseY];
+  console.log(ogMousePos);
+
+  document.body.addEventListener("mouseleave", function(event) {
+    canvasActive = false;
+  });
+  document.body.addEventListener("mouseenter", function(event) {
+    canvasActive = true;
+  });
+
+  fill(c, 255, 255);
+  ellipse(xPos, yPos, ellipseWidth, ellipseWidth);
 }
 
 function draw() {
+  if (!canvasActive) {
+    return;
+  }
+  if (mousehasntMoved) {
+    if (JSON.stringify([mouseX, mouseY]) !== JSON.stringify(ogMousePos)) {
+      mousehasntMoved = false;
+    }
+  }
+  if (mousehasntMoved) {
+    return;
+  }
   const totChange = sqrt((mouseX - xPos) ** 2 + (mouseY - yPos) ** 2);
 
   const xChange = (mouseX - xPos) / totChange;
@@ -30,14 +60,9 @@ function draw() {
   xPos += xChange * movement;
   yPos += yChange * movement;
 
-  if (abs(xChange * movement) + abs(yChange * movement) > 1) {
-    if (rainbowRaising) {
-      if (c >= 255) rainbowRaising = false;
-      else c += 0.5;
-    } else {
-      if (c <= 0) rainbowRaising = true;
-      else c -= 0.5;
-    }
+  if (mouseX - xPos + (mouseY - yPos) > 1) {
+    if (c >= 255) c = 0;
+    else c += 0.5;
   }
   fill(c, 255, 255);
 
@@ -48,4 +73,5 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   xPos = windowWidth / 2;
   yPos = windowHeight / 2;
+  clear();
 }
